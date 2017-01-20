9f9245ac7d612d699da34b07470c1bc2d1e6c8e0
haroldwongms <harold.wong@microsoft.com>
2017-01-19 17:31:18 -0700

    Update for OpenShift Container Platform 3.4
    Install 3.4
    Install Cockpit

--- scripts/nodePrep.sh
+++ scripts/nodePrep.sh
@@ -8,7 +8,7 @@ POOL_ID=$3
 # Register Host with Cloud Access Subscription
 echo $(date) " - Register host with Cloud Access Subscription"
 
-subscription-manager register --username=$USER --password=$PASSWORD
+subscription-manager register --username="$USER" --password="$PASSWORD"
 subscription-manager attach --pool=$POOL_ID
 
 # Disable all repositories and enable only the required ones
@@ -19,7 +19,15 @@ subscription-manager repos --disable="*"
 subscription-manager repos \
     --enable="rhel-7-server-rpms" \
     --enable="rhel-7-server-extras-rpms" \
-    --enable="rhel-7-server-ose-3.3-rpms"
+    --enable="rhel-7-server-ose-3.4-rpms"
+
+# Install and enable Cockpit
+echo $(date) " - Installing and enabling Cockpit"
+
+yum -y install cockpit
+
+systemctl enable cockpit.socket
+systemctl start cockpit.socket
 
 # Install base packages and update system to latest packages
 echo $(date) " - Install base packages and update system to latest packages"
@@ -27,10 +35,10 @@ echo $(date) " - Install base packages and update system to latest packages"
 yum -y install wget git net-tools bind-utils iptables-services bridge-utils bash-completion
 yum -y update --exclude=WALinuxAgent
 
-# Install Docker 1.10.3
-echo $(date) " - Installing Docker 1.10.3"
+# Install Docker 1.12
+echo $(date) " - Installing Docker 1.12"
 
-yum -y install docker-1.10.3
+yum -y install docker
 sed -i -e "s#^OPTIONS='--selinux-enabled'#OPTIONS='--selinux-enabled --insecure-registry 172.30.0.0/16'#" /etc/sysconfig/docker
 
 # Create thin pool logical volume for Docker
