ea7eaf4eb1e0676c9fe96f284e0a0aaa30d6fe5c
haroldwongms <harold.wong@microsoft.com>
2017-01-27 16:01:06 -0700

    Cleaner way to find newly attached disk for Docker

--- scripts/nodePrep.sh
+++ scripts/nodePrep.sh
@@ -59,7 +59,7 @@ sed -i -e "s#^OPTIONS='--selinux-enabled'#OPTIONS='--selinux-enabled --insecure-
 # Create thin pool logical volume for Docker
 echo $(date) " - Creating thin pool logical volume for Docker and staring service"
 
-DOCKERVG=$( sfdisk -l | grep -E '16709|66837|133544' | awk -F ":" '{ print $1 }' | awk '{ print $2 }' )
+DOCKERVG=$( parted -m /dev/sda print all 2>/dev/null | grep unknown | cut -d':' -f1 )
 
 echo "DEVS=${DOCKERVG}" >> /etc/sysconfig/docker-storage-setup
 echo "VG=docker-vg" >> /etc/sysconfig/docker-storage-setup
