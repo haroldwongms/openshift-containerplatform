c4b41961b06b1eae102dcc6b94ce55a60275d6e7
Harold Wong <harold.wong@microsoft.com>
2017-02-20 15:09:10 -0700

    Combined Template (#11)
    Combined Template: Allows use of Custom VHD Image or Azure Gallery Image

--- scripts/bastionPrep.sh
+++ scripts/bastionPrep.sh
@@ -1,29 +1,42 @@
 #!/bin/bash
 echo $(date) " - Starting Script"
 
-ORG=$1
-ACT_KEY="$2"
-POOL_ID=$3
+SELECT=$1
+USERNAME_ORG=$2
+PASSWORD_ACT_KEY="$3"
+POOL_ID=$4
 
 # Register Host with Cloud Access Subscription
 echo $(date) " - Register host with Cloud Access Subscription"
 
-subscription-manager register --org="$ORG" --activationkey="$ACT_KEY"
+if [[ $SELECT == "usernamepassword" ]]
+then
+   subscription-manager register --username="$USERNAME_ORG" --password="$PASSWORD_ACT_KEY"
+else
+   subscription-manager register --org="$USERNAME_ORG" --activationkey="$PASSWORD_ACT_KEY"
+fi
+
 if [ $? -eq 0 ]
 then
    echo "Subscribed successfully"
 else
-   echo "Incorrect Organization ID and / or Activation Key specified"
+   echo "Incorrect Username and Password or Organization ID and / or Activation Key specified"
    exit 3
 fi
 
-subscription-manager attach --pool=$POOL_ID
+subscription-manager attach --pool=$POOL_ID > attach.log
 if [ $? -eq 0 ]
 then
    echo "Pool attached successfully"
 else
-   echo "Incorrect Pool ID or no entitlements available"
-   exit 4
+   evaluate=$( cut -f 2-5 -d ' ' attach.log )
+   if [[ $evaluate == "unit has already had" ]]
+      then
+         echo "Pool $POOL_ID was already attached and was not attached again."
+	  else
+         echo "Incorrect Pool ID or no entitlements available"
+         exit 4
+   fi
 fi
 
 # Disable all repositories and enable only the required ones
